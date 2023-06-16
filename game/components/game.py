f91ba78a624e889d51a1fa4c63a3730ac26e3c05
Oscar1623 <oramosp1400@alumno.ipn.mx>
2023-06-16 14:42:02 -0600

    Tarea 1 - inicio

--- game/components/game.py
+++ game/components/game.py
@@ -1,8 +1,11 @@
 import pygame
 
 from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+from game.components.spaceship import Spaceship
+#Game tiene un spaceship (una instancia de una clse Spaceship)
 
-
+#Game puede decirle al spaceship que se actualize llamando al metodo update(), update espera una lista que contiene
+#los eventos del teclado que pudieron haber ocurrido
 class Game:
     def __init__(self):
         pygame.init()
@@ -14,6 +17,7 @@ class Game:
         self.game_speed = 10 # el numero de pixeles que el "objeto / imagen" se mueve en patalla
         self.x_pos_bg = 0
         self.y_pos_bg = 0
+        self.spaceship = Spaceship()
 
     def run(self):
         # Game loop: events - update - draw
@@ -34,12 +38,19 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass # pass equivale a hacer nada 
+        #el update llama al update de los objetos de algunos de los objetos de mi juego
+        #pass # pass equivale a hacer nada 
+        events = pygame.key.get_pressed() #contiene todos los eventos del teclado qye pudieron ocurrir en un gameloop
+        self.spaceship.update(events)
 
     def draw(self):
         self.clock.tick(FPS) # configuro cuantos frames per second voy a dibujar
         self.screen.fill((255, 255, 255)) # lleno el screen de color BLANCO???? 255, 255, 255 es el codigo RGB
+        #yo "Game" se dibujar mi escenario
         self.draw_background()
+        #yo "Game" le ordeno al spaceship dibujarse llamando a un metodo draw del Spaceship
+        #el metodo draw espera que le pase el screen
+        self.spaceship.draw(self.screen)
         pygame.display.update() # esto hace que el dibujo se actualice en el display de pygame
         pygame.display.flip()  # hace el cambio
 
---
+++ game/components/spaceship.py
@@ -0,0 +1,17 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+from game.utils.constants import SPACESHIP
+#sprite es un objeto de pygame
+class Spaceship(Sprite):
+    def __init__(self):
+        self.image = SPACESHIP
+        self.image = pygame.transform.scale(self.image,(50,60)) #ancho y alto
+        self.rect = self.image.get_rect()
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def update(self,keyboard_events):
+        pass
